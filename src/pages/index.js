aee00380a26451f60c0f34fcd5bafabfc157f7f3
kennyhw <kennyhermawan7@gmail.com>
2020-05-27 00:52:56 +0800

    Add components and bootstrap grid system

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,29 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import Header from "../components/header";
+import About from "../components/about";
+import Projects from "../components/projects";
+import Contact from "../components/contact";
+
+import "../styles/bootstrap/bootstrap-grid.min.css"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <SEO title="Kenny Hermawan" />
+    <section>
+      <Header />
+    </section>
+    <section>
+      <About />
+    </section>
+    <section>
+      <Projects />
+    </section>
+    <section>
+      <Contact />
+    </section>
   </Layout>
 )
 
