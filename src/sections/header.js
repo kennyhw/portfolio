1dfc54e7584e67a103d1fd26f928ffc26cc24739
kennyhw <kennyhermawan7@gmail.com>
2020-06-24 18:14:28 +0800

    Add photo and modify header layout

--- src/sections/header.js
+++ src/sections/header.js
@@ -1,28 +1,45 @@
 import React from "react"
 
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 import scrollTo from "gatsby-plugin-smoothscroll"
 
 import styles from "../styles/header.module.css"
 
 function Header() {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        file(relativePath: { eq: "assets/profile-picture.png" }) {
+          childImageSharp {
+            fixed(width: 175, height: 175) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+      }
+    `
+  );
+
   return (
     <section className={styles.sectionContainer}>
       <div className="row">
         <div className={'col-12 ' + styles.picContainer}>
-          <svg width="150" height="150">
-            <circle cx="75" cy="75" r="75" fill="#e41376" />
-          </svg>
+          <Img className={styles.profilePicture} fixed={data.file.childImageSharp.fixed} alt="Photo" draggable="False"/>
         </div>
       </div>
       <div className="row">
         <div className={'col-12 ' + styles.textContainer}>
-          <span className={styles.name}>Kenny Hermawan<span className={styles.nameDot}>.</span></span><br></br>
-          <span className={styles.desc}>Aspiring Software Developer | Undergraduate @ NUS</span><br></br>
+          <span className={styles.name}>Kenny Hermawan<span className={styles.nameDot}>.</span></span>
+          <br></br>
+          <span className={styles.desc}>Aspiring Software Developer | Undergraduate @ NUS</span>
         </div>
       </div>
       <div className="row">
         <div className={'col-12 ' + styles.arrowContainer}>
-          <svg className={styles.arrowDown} onClick={() => scrollTo("#about-me")} xmlns="http://www.w3.org/2000/svg" width="35.924" height="17.962" viewBox="0 0 35.924 17.962">
+          <svg className={styles.arrowDown} onClick={() => scrollTo("#about-me")} 
+              xmlns="http://www.w3.org/2000/svg" width="35.924" height="17.962" 
+              viewBox="0 0 35.924 17.962">
             <g>
               <polygon points="17.962 17.962 35.924 0 0 0 17.962 17.962"/>
             </g>
