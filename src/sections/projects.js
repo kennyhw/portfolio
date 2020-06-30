50a3ad680c6fe57ff8090aa878e673269ea69d5d
kennyhw <kennyhermawan7@gmail.com>
2020-06-30 22:28:00 +0800

    Modify 'projects' section

--- src/sections/projects.js
+++ src/sections/projects.js
@@ -1,7 +1,26 @@
 import React from "react"
+
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+import { GoLink, GoRepo } from "react-icons/go"
+
 import styles from "../styles/projects.module.css"
 
 function Projects() {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        file(relativePath: { eq: "assets/projects-image/01-checkmark.png" }) {
+          childImageSharp {
+            fixed(width: 300, height: 220) {
+              ...GatsbyImageSharpFixed_noBase64
+            }
+          }
+        }
+      }
+    `
+  );
+
   return (
     <section className={styles.sectionContainer}>
       <div className="row">
@@ -10,40 +29,34 @@ function Projects() {
         </div>
       </div>
       <div className="row">
-        <div className="col-4">
-          <div className={styles.box}>
-            <svg width="275" height="110">
-              <rect width="275" height="110" fill="#e42154" />
-            </svg>
-            <h3 className={styles.projectTitle}>Title</h3>
-            <p className={styles.projectDesc}>
-              Description description description Description description description Description description description
-              Description description description Description description description
-          </p>
+        <div className="col-md-4">
+          <div className={styles.projectContainer}>
+          <Img className={styles.projectImage} fixed={data.file.childImageSharp.fixed} alt="CheckMark" draggable="False"/>
+            <span className={styles.projectTitle}>CheckMark</span><br></br>
+            <span className={styles.projectDesc}>A to-do list application built with React and Ruby on Rails</span><br></br>
+            <a className={styles.projectLinkIconAnchor} href=""><GoLink className={styles.projectLinkIcon} /></a>
+            <a className={styles.projectLinkIconAnchor} href=""><GoRepo className={styles.projectLinkIcon} /></a>
           </div>
         </div>
-        <div className="col-4">
-          <div className={styles.box}>
-            <svg width="275" height="110">
-              <rect width="275" height="110" fill="#e42154" />
+        <div className="col-md-4">
+          <div className={styles.projectContainer}>
+            <svg width="400" height="220">
+              <rect width="300" height="220" />
             </svg>
-            <h3 className={styles.projectTitle}>Title</h3>
-            <p className={styles.projectDesc}>
-              Description description description Description description description Description description description
-              Description description description Description description description
-          </p>
+            <h3>Title</h3>
+            <p>
+              Description
+            </p>
           </div>
-        </div>
-        <div className="col-4">
-          <div className={styles.box}>
-            <svg width="275" height="110">
-              <rect width="275" height="110" fill="#e42154" />
+        </div><div className="col-md-4">
+          <div className={styles.projectContainer}>
+            <svg width="400" height="220">
+              <rect width="300" height="220" />
             </svg>
-            <h3 className={styles.projectTitle}>Title</h3>
-            <p className={styles.projectDesc}>
-              Description description description Description description description Description description description
-              Description description description Description description description
-          </p>
+            <h3>Title</h3>
+            <p>
+              Description
+            </p>
           </div>
         </div>
       </div>
