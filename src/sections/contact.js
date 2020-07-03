af12fc46a73003e218e9a2ceb5379aace3aa206d
kennyhw <kennyhermawan7@gmail.com>
2020-07-03 16:40:28 +0800

    Add new site icon and modify 'contacts' section

--- src/sections/contact.js
+++ src/sections/contact.js
@@ -1,12 +1,44 @@
 import React from "react"
+
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
 import styles from "../styles/contact.module.css"
 
+/*
+ * Page footer
+ */
 function Contact() {
-  return(
+  const data = useStaticQuery(
+    graphql`
+      query {
+        file(relativePath: { eq: "assets/logo.png" }) {
+          childImageSharp {
+            fixed(width: 48, height: 48) {
+              ...GatsbyImageSharpFixed_noBase64
+            }
+          }
+        }
+      }
+    `
+  );
+
+  return (
     <section className={styles.sectionContainer}>
       <div className="row">
         <div className="col-12">
-          <h3 className={styles.flair}>Contacts</h3>
+          <Img className={styles.siteLogo} fixed={data.file.childImageSharp.fixed} alt="Site Logo" draggable="False" /><br></br>
+          <div className={styles.contactContainer}>
+            <span className={styles.contactDetails}>
+              Get in touch!<br></br>
+              <span className={styles.contactEmail}>kennyhermawan7@gmail.com</span>
+            </span>
+          </div>
+          <div className={styles.footerContainer}>
+            <span className={styles.footerDetails}>
+              Copyright
+            </span>
+          </div>
         </div>
       </div>
     </section>
